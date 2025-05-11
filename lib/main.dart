017f8cb7beb71af3ae1700fa1356cc32cc66bc65
sadiqaikas <55606393+sadiqaikas@users.noreply.github.com>
2025-05-11 15:46:38 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,6 @@ class _MathSolverScreenState extends State<MathSolverScreen> {
   final PageController _pageController = PageController();
   bool _showMathKeyboard = false;
 
-  final String apiKey = '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'; // Replace with your actual API key
 
   @override
   void initState() {
